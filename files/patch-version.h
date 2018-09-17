40565b870bf82d5d4b47a79893be438f2ee3e640
dmn <graahnul.grom@gmail.com>
2018-09-17 14:05:21 +0300

    add version.h patch to files/...
    somwhow this file is absent in the 1.9.5-20180820
    "Source archive" which is downloaded from github.
    w/o it compilation fails

---
+++ files/patch-version.h
@@ -0,0 +1,11 @@
+--- version.h.orig	2018-09-17 10:22:58 UTC
++++ version.h
+@@ -0,0 +1,8 @@
++/* Date part of package version string. */
++#define PACKAGE_DATE_VERSION "20180820"
++
++/* Dotted part of package version string. */
++#define PACKAGE_DOTTED_VERSION "1.9.5"
++
++/* Current git commit. */
++#define PACKAGE_GIT_COMMIT "6b1721cfe4f54d7856c0d3c0c3ac4946730947a4"
