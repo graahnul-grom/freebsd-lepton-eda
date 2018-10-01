5439ebfa4893695ac274fde84c3f2f54e50f4429
dmn <graahnul.grom@gmail.com>
2018-10-01 15:29:56 +0300

    fix version.h patch - correct git revision

--- files/patch-version.h
+++ files/patch-version.h
@@ -1,5 +1,5 @@
---- version.h.orig	2018-09-17 10:22:58 UTC
-+++ version.h
+--- /dev/null	2018-10-01 15:13:26.000000000 +0300
++++ version.h	2018-08-20 23:22:20.000000000 +0300
 @@ -0,0 +1,8 @@
 +/* Date part of package version string. */
 +#define PACKAGE_DATE_VERSION "20180820"
@@ -8,4 +8,4 @@
 +#define PACKAGE_DOTTED_VERSION "1.9.5"
 +
 +/* Current git commit. */
-+#define PACKAGE_GIT_COMMIT "6b1721cfe4f54d7856c0d3c0c3ac4946730947a4"
++#define PACKAGE_GIT_COMMIT "a7054e11ce0ef3cc5791ce6d3b186ba3dfcb673c"
