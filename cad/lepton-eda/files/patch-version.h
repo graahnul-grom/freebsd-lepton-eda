dbeb9fb5447cfd3c647bcaae49dcaaacab015226
dmn <graahnul.grom@gmail.com>
2018-10-08 15:29:20 +0300

    add "production" cad/lepton-eda/ (copy of wip files)

---
+++ cad/lepton-eda/files/patch-version.h
@@ -0,0 +1,11 @@
+--- version.h.orig	2018-10-08 11:02:32 UTC
++++ version.h
+@@ -0,0 +1,8 @@
++/* Date part of package version string. */
++#define PACKAGE_DATE_VERSION "20180820"
++
++/* Dotted part of package version string. */
++#define PACKAGE_DOTTED_VERSION "1.9.5"
++
++/* Current git commit. */
++#define PACKAGE_GIT_COMMIT "a7054e11ce0ef3cc5791ce6d3b186ba3dfcb673c"
