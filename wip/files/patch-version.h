c95feeb137805520f467139c35302633d0ccf0a7
dmn <graahnul.grom@gmail.com>
2018-11-02 12:26:26 +0300

    fix patch-version.h...
    1) cd /usr/ports/cad/lepton-eda/
    2) make clean && make extract
    3) cd work/lepton-eda-1.9.6-20181101/
    4) echo -n > version.h.orig
    5) checkout revision 7f12aab (in $lepton/lepton.git/)
    6) cp $lepton/lepton.git/version.h .
    7) cd ../..
    8) make makepatch

--- wip/files/patch-version.h
+++ wip/files/patch-version.h
@@ -1,11 +1,11 @@
---- version.h.orig	2018-11-02 09:06:53 UTC
+--- version.h.orig	2018-11-02 09:20:44 UTC
 +++ version.h
-@@ -1,8 +0,0 @@
--/* Date part of package version string. */
--#define PACKAGE_DATE_VERSION "20181101"
--
--/* Dotted part of package version string. */
--#define PACKAGE_DOTTED_VERSION "1.9.6"
--
--/* Current git commit. */
--#define PACKAGE_GIT_COMMIT "7f12aabd0b522f42e3ad546cd52476dab0953e13"
+@@ -0,0 +1,8 @@
++/* Date part of package version string. */
++#define PACKAGE_DATE_VERSION "20181101"
++
++/* Dotted part of package version string. */
++#define PACKAGE_DOTTED_VERSION "1.9.6"
++
++/* Current git commit. */
++#define PACKAGE_GIT_COMMIT "7f12aabd0b522f42e3ad546cd52476dab0953e13"
