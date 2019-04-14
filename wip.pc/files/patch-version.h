6ae09f01fad6c3006d7c4603d25877bf9482cc1e
dmn <graahnul.grom@gmail.com>
2019-04-14 19:58:17 +0300

    add wip.pc/: pre-compilation support...
    cad/lepton-eda FreeBSD port built with
    pre-compilation support.
    
    Built from:
    - tb_119_precompile branch:
    https://github.com/graahnul-grom/lepton-eda/tree/tb_119_precompile
    - commit:
    090731f0aba0488699007634cede5c94a431a408

---
+++ wip.pc/files/patch-version.h
@@ -0,0 +1,11 @@
+--- version.h.orig	2018-12-13 02:49:55 UTC
++++ version.h
+@@ -0,0 +1,8 @@
++/* Date part of package version string. */
++#define PACKAGE_DATE_VERSION "20181211"
++
++/* Dotted part of package version string. */
++#define PACKAGE_DOTTED_VERSION "1.9.7"
++
++/* Current git commit. */
++#define PACKAGE_GIT_COMMIT "090731f0aba0488699007634cede5c94a431a408"
