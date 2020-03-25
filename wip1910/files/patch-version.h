2261c7541bad7fd30a98310facfe4c64fc994fc8
dmn <graahnul.grom@gmail.com>
2020-03-25 08:40:51 +0300

    wip1910: patch: version.h

---
+++ wip1910/files/patch-version.h
@@ -0,0 +1,11 @@
+--- version.h.orig	2020-03-25 05:39:40 UTC
++++ version.h
+@@ -0,0 +1,8 @@
++/* Date part of package version string. */
++#define PACKAGE_DATE_VERSION "20200319"
++
++/* Dotted part of package version string. */
++#define PACKAGE_DOTTED_VERSION "1.9.10"
++
++/* Current git commit. */
++#define PACKAGE_GIT_COMMIT "c69155b34b82d2b288f4787f92fe930112e73c72"
